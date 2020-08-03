4bc23cf52e532733014a38bcecd0301eaf7689a4
Adam Eshel <adameshel@mail.tau.ac.il>
2020-08-03 18:08:15 +0300

    adding the radar option and working on it

--- kriging.py
+++ kriging.py
@@ -13,6 +13,7 @@ import pandas as pd
 from scipy.spatial.distance import pdist, squareform
 from scipy.optimize import curve_fit
 import sys
+import math
  
 def SVh( P, h, bw ):
     '''
@@ -145,13 +146,14 @@ def krige( P, u, N, covfct, Kinverse ):
  
     return estimation*(estimation>0), weights
 
-
-
+##########################################################################
+#### Execute this function to get semivariogram from cml measurements ####
+##########################################################################
 def kriging_execute(df_krg, x_grid_utm, y_grid_utm, 
                     sill_guess=None, range_guess=25, range_cal=False,
                     timestamp=0, unit_length='m', delete_links=True, 
                     change_bw=False, bandwidth_in_km=0.3, 
-                    doy=1, duration_agg=186, printing=False):
+                    doy=1, duration_agg=186, optimize=True, printing=False):
     '''df_krg: dataframe which is in the form of the output of
 `create_virtual_gauges.py`.
 range_guess: a first guess of the decorrelation distance (in km).
@@ -165,6 +167,7 @@ change_bw: if True- increases the band width while the
 cov. condition number is too large.
 doy: the Day Of Year Parameter from Van De Beek 2012.
 duration_agg: duration of aggrigation time of rainfall (h). Van De Beek 2012.
+optimize: Bool. Optimize kriging parameters or not.
 '''
     if unit_length == 'm':
         unit_helper = 1e3
@@ -302,17 +305,23 @@ duration_agg: duration of aggrigation time of rainfall (h). Van De Beek 2012.
         
         sv[1] = sv[1] / magnitude_sill
         sill_guess = sill_guess / magnitude_sill
- 
-        try:
-            popt, pcov = curve_fit(f=func_to_opt, xdata=sv[0], ydata=sv[1], 
-                                   bounds=(0, [sill_guess, range_guess])) 
-            C0, a_krg = popt # C0 is the sill and a_krg is the range
+        
+        if optimize is True:
+     
+            try:
+                popt, pcov = curve_fit(f=func_to_opt, xdata=sv[0], ydata=sv[1], 
+                                       bounds=(0, [sill_guess, range_guess])) 
+                C0, a_krg = popt # C0 is the sill and a_krg is the range
 
-        except:
+            except:
+                a_krg = range_guess
+                C0 = sill_guess
+                print('OPT func. could not converge. First guesses were used for'\
+                      + ' Sill and Range.')
+        else:
             a_krg = range_guess
             C0 = sill_guess
-            print('OPT func. could not converge. First guesses were used for'\
-                  + ' Sill and Range.')
+        
         a_krg = a_krg * unit_helper * magnitude_range
         sv[0] = sv[0] * unit_helper * magnitude_range
         
@@ -397,6 +406,190 @@ duration_agg: duration of aggrigation time of rainfall (h). Van De Beek 2012.
     df_krg = df_krg.reset_index()
     print('Processing finished')
     
-    return gridded_snapshot, df_krg, [a_krg, C0]
+    return gridded_snapshot, df_krg, (a_krg, C0)
+#######################################################
+#######################################################
+    
+
+##########################################################################
+##### Execute this function to get semivariogram from a gridded radar ####
+##########################################################################
+def kriging_grid_execute(rain_xarray,
+                    sill_guess=None, range_guess=25, range_cal=False,
+                    timestamp=0, unit_length='m', change_min_dist=True, 
+                    change_bw=False, bandwidth_in_km=1.0, 
+                    doy=1, duration_agg=186, optimize=True, printing=False):
+    '''rain_xarray: single time-stamp xarray with rainfall values named 
+    `raindepth`.
+    range_guess: a first guess of the decorrelation distance (in km).
+    range_cal: if True, the first guess of range is derived from doy and 
+    duration_agg.
+    timestamp: index of target timestamp
+    unit_length: the units of x_grid_utm, y_grid_utm (usually 'm').
+    change_min_dist: if True- deletes links by distances from one another
+    while the cov. condition number is too large.
+    change_bw: if True- increases the band width while the
+    cov. condition number is too large.
+    doy: the Day Of Year Parameter from Van De Beek 2012.
+    duration_agg: duration of aggrigation time of rainfall (h). Van De Beek 2012.
+    optimize: Bool. Optimize kriging parameters or not.
+    '''
+    x_grid_utm = rain_xarray.x_utm.values
+    y_grid_utm = rain_xarray.y_utm.values
+    p = np.array([np.ravel(x_grid_utm),
+                np.ravel(x_grid_utm),
+                np.ravel(rain_xarray.raindepth.isel(time=timestamp))]).T
+    df_p = pd.DataFrame(p, columns=['x','y','z'])
+    
+    if unit_length == 'm':
+        unit_helper = 1e3
+    else:
+        unit_helper = 1
+    xMax = np.nanmax(rain_xarray.x_utm.values)
+    xMin = np.nanmin(rain_xarray.x_utm.values)
+    yMax = np.nanmax(rain_xarray.y_utm.values)
+    yMin = np.nanmin(rain_xarray.y_utm.values)
+
+
+
+    K_matCond = 1e9
+#     dist_spacer = sys.float_info.epsilon
+    dist_spacer = sys.float_info.epsilon # meters
+    Cond = 0
+    bw_helper = 0 # km
+    cond_num_thresh = 3e5
+    while K_matCond >= cond_num_thresh:
+        bw = bandwidth_in_km * unit_helper + bw_helper * unit_helper # bandwidth in km
+        hs_factor = 0.75
+        hs = np.arange(0, np.min([np.max([xMax-xMin,yMax-yMin]), 50*unit_helper])\
+                       * hs_factor, bw*2.0)
+        print(Cond)
+        print('bw: ' + str(bw))
+
+#        p = df_p[['x','y','z']].values
+        sv = SV( p, hs, bw )
+        ## optimization of params
+        if range_cal is True: # use the Van Der Beek range parameter
+            range_guess = ((15.51*duration_agg**0.09+2.06*\
+                            duration_agg**(-0.12)*\
+                            np.cos((2*np.pi*(doy-7.37*duration_agg**0.22))/\
+                                   365))**4)/1000
+        if sill_guess is None:
+            print('here')
+            print(sv)
+#            sill_guess = np.median(sv[1]) + np.median(sv[1]) / 2
+            sill_guess = np.var(sv[1])
+            if (sill_guess < 0.5) or (sill_guess is None):
+                sill_guess = 0.5
+            print(sill_guess)
+        ## normalizing the initial guesses and the semivariance values
+        magnitude_sill = 10 ** (int(np.log10(sill_guess)))
+        magnitude_range = 10 ** (int(np.log10(range_guess)))
+        
+        sv[0] = sv[0] / unit_helper 
+        sv[0] = sv[0] / magnitude_range
+#        range_guess = np.min([range_guess / magnitude_range, sv[0][-1]])
+        range_guess = range_guess / magnitude_range
+        
+        sv[1] = sv[1] / magnitude_sill
+        sill_guess = sill_guess / magnitude_sill
+        
+        if optimize is True:
+     
+            try:
+                popt, pcov = curve_fit(f=func_to_opt, xdata=sv[0], ydata=sv[1], 
+                                       bounds=(0, [sill_guess, range_guess])) 
+                C0, a_krg = popt # C0 is the sill and a_krg is the range
+
+            except:
+                a_krg = range_guess
+                C0 = sill_guess
+                print('OPT func. could not converge. First guesses were used for'\
+                      + ' Sill and Range.')
+        else:
+            a_krg = range_guess
+            C0 = sill_guess
+        
+        a_krg = a_krg * unit_helper * magnitude_range
+        sv[0] = sv[0] * unit_helper * magnitude_range
+        
+        C0 = C0 * magnitude_sill
+        sv[1] = sv[1] * magnitude_sill
+#        import pdb; pdb.set_trace()
+######################################################################
+######################################################################
+#         C0=SILL; a_krg=RANGE
+######################################################################
+######################################################################
+#             import pdb; pdb.set_trace()
+        noise_in_function = False
+        cov_func = lambda h: C0 - func_to_opt( h, C0, a_krg )
+#        cov_func = lambda h: 0 if (h > a_krg) else (C0 - func_to_opt( h, C0, a_krg ))
+#        import pdb; pdb.set_trace()
+        # number of neighboring points to consider
+        N = np.shape(p)[0]
+        # form a matrix of distances between existing data points
+        K = squareform( pdist( df_p[['x','y']] ) )
+        #apply the covariance model to these distances
+#        import pdb; pdb.set_trace()
+        K = cov_func( K.ravel() )
+        K = np.array( K )
+        K = K.reshape( N,N )
+        K_matCond = np.linalg.cond( K ) # condition number
+        
+        if K_matCond > cond_num_thresh:
+            noise_in_function = True
+            cov_func = lambda h: C0 - func_with_noise( h, C0, a_krg )
+    #        import pdb; pdb.set_trace()
+            # number of neighboring points to consider
+            N = np.shape(p)[0]
+            # form a matrix of distances between existing data points
+            K = squareform( pdist( df_p[['x','y']] ) )
+            #apply the covariance model to these distances
+            K = cov_func( K.ravel() )
+            K = np.array( K )
+            K = K.reshape( N,N )
+            K_matCond = np.linalg.cond( K ) # condition number
+            
+        Cond = Cond + 1
+        bw_helper += 0.05 * change_bw
+        print('K_matCond: ' + str(K_matCond))
+
+    K_inv = np.linalg.inv( K )
+
+    print('condition number = ' + str(K_matCond) + '\n', 'dist_spacer = ' + \
+          str(dist_spacer) + '\n', 'C0 = ' + str(C0) + '\n', 'a_krg = ' + \
+          str(a_krg) + '\n',\
+          'bandwidth (bw) = ' + str(bw) + '\n', 'noise_in_cov_function = ' +\
+          str(noise_in_function) + '\n')
+    if printing is True:
+        ## plotting the semivariogram
+        plt.fig, ax = plt.subplots(figsize=(10,5))
+        ax.plot( sv[0], sv[1], '.-' )
+        plt.xlim(0,np.max(sv[0]))
+        ax.plot( sv[0], func_to_opt( sv[0], C0, a_krg ) )
+        ax.plot( sv[0], cov_func(sv[0]) )
+        ax.set_title('Spherical Model, timeframe: ' + str(timestamp))
+        ax.set_ylabel('Semivariance')
+        ax.set_xlabel('Lag [m]')
+    
+    rainfield_flat = np.zeros(np.shape(x_grid_utm.flatten()))
+#         temp_weights = np.zeros(np.shape(py_xgrid_UTM.flatten()))
+    for i, (xi, yj) in enumerate(zip(x_grid_utm.flatten(),
+                                 y_grid_utm.flatten())):
+        rainfield_flat[i], _ = krige( p, (xi,yj), N, cov_func, K_inv)
+    gridded_snapshot = rainfield_flat.reshape(np.shape(x_grid_utm))
+
+    print(' max rain intensity estimated = ' + str(np.max(gridded_snapshot)))
+
+
+    if 'index' in df_p.columns:
+        df_p.drop('index', axis=1, inplace=True)
+    if 'level_0' in df_p.columns:
+        df_p.drop('level_0', axis=1, inplace=True)
+    df_p = df_p.reset_index()
+    print('Processing finished')
+    
+    return gridded_snapshot, df_p, (a_krg, C0)
 #######################################################
 #######################################################
